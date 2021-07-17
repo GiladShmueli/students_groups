8b577e88dfff1f59bbf1035c7f7187f81e36c310
ilyag93 <80205676+ilyag93@users.noreply.github.com>
2021-07-17 19:28:38 +0300

    works a little better

--- app.js
+++ app.js
@@ -2,65 +2,34 @@ const questionList = document.querySelector('#question-list');
 
 // create element & render test
 function renderTest(doc){
+
     let qdiv = document.createElement('div');
     let question = document.createElement('p');
     let options = document.createElement('div')
-
-    let option1 = document.createElement('input');
-    option1.setAttribute("type", "radio");
-    option1.setAttribute("name", doc.id);
-
-
-    let option2 = document.createElement('input');
-    option2.setAttribute("type", "radio");
-    option2.setAttribute("name", doc.id);
-
-    let option3 = document.createElement('inupt');
-    option3.setAttribute("type", "radio");
-    option3.setAttribute("name", doc.id);
-
-    let option4 = document.createElement('input');
-    option4.setAttribute("type", "radio");
-    option4.setAttribute("name", doc.id);
-
-
-    qdiv.setAttribute('data-id', doc.id);
+    qdiv.id = doc.id;
     question.textContent = doc.data().question;
-    option1.id = doc.data().options[0];
-    option2.id = doc.data().options[1];
-    option3.id = doc.data().options[2];
-    option4.id = doc.data().options[3];
-
-    let lb1 = document.createElement("label");
-    lb1.textContent = doc.data().options[0];
-    lb1.setAttribute("for",option1.id);
-
-    let lb2 = document.createElement("label");
-    lb2.textContent = doc.data().options[1];
-    lb2.setAttribute("for",option2.id);
-
-    let lb3 = document.createElement("label");
-    lb3.textContent = doc.data().options[2];
-    lb3.setAttribute("for",option3.id);
-
-    let lb4 = document.createElement("label");
-    lb4.textContent = doc.data().options[3];
-    lb4.setAttribute("for",option4.id);
-
     qdiv.appendChild(question);
-    options.appendChild(option1);
-    options.appendChild(lb1);
 
-    options.appendChild(option2);
-    options.appendChild(lb2);
+    for(let i = 0; i < 4; i++)
+    {
+        let option1 = document.createElement('input');
+        option1.type= "radio";
+        option1.name= doc.id;
+        option1.id = "ans"+ i + " " + doc.id;
 
-    options.appendChild(option3);
-    options.appendChild(lb3);
+        let lb1 = document.createElement("label");
+        let description1 = document.createTextNode(doc.data().options[i]);
+        //lb1.textContent = doc.data().options[0];
+        lb1.appendChild(description1);
+        lb1.setAttribute("for","ans"+ i + " " + doc.id);
 
-    options.appendChild(option4);
-    options.appendChild(lb4);
+        let br1 = document.createElement('br');
 
-    qdiv.appendChild(options);
+        qdiv.appendChild(option1);
+        qdiv.appendChild(lb1);
+        qdiv.appendChild(br1);
+    }
+    
     questionList.appendChild(qdiv);
 }
 
