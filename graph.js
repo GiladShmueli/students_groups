d86ebd25d3d7f00d7d5b0a8fa8f5a34f603b0f8e
GiladShmueli <33592872+GiladShmueli@users.noreply.github.com>
2021-08-15 16:24:45 +0300

    basic graph + test with randomization

---
+++ graph.js
@@ -0,0 +1,86 @@
+let test_results = [98, 29.8, 48, 29, 29,
+                    40, 70.42, 80, 81.34, 89,
+                    
+                    22.17, 55, 55,
+                    66, 34, 56, 88, 99,
+                    0, 100, 0, 0, 100];
+test_results.sort((a, b) => a - b); //using lambda, otherwise sorts as strings, 100 is definitely not before 20 ;)
+
+
+const WIDTH = 420;
+const HEIGHT = 300;
+const PADDING = 10.5;
+const TXTZONE = 22;
+
+createGraphPNG(test_results);
+function createGraphPNG(test_results) {
+    const tests_cnt = test_results.length;
+    let canvas = document.createElement("canvas");
+    canvas.width = WIDTH + TXTZONE + 2 * PADDING;
+    canvas.height = HEIGHT + 3 * PADDING;
+    let ctx = canvas.getContext("2d");
+    let x, y;
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    drawGrid(ctx, tests_cnt, 10);
+    ctx.fillStyle = "#55AAEE";
+    ctx.strokeStyle = "#55AAEE";
+    
+    for (let i = 0; i < tests_cnt; i++) {
+        makeCircle(x, y, ctx);
+        x = PADDING + TXTZONE + i * WIDTH / tests_cnt;
+        y = HEIGHT + PADDING - test_results[i] * 3;
+        drawLine(x, y, ctx);
+
+    }
+    makeCircle(x, y, ctx);
+
+    var img = canvas.toDataURL("image.png");
+    document.write('<img id="grades_graph" src="' + img + '" />');
+}
+
+//document.body.appendChild(canvas);
+
+function drawLine(x, y, ctx) {  
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
+
+function makeCircle(x, y, ctx) {
+    ctx.beginPath();
+    ctx.arc(x, y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function drawGrid(ctx, xlines, ylines) {
+    let x, y;
+    ctx.strokeStyle = "#EEDDFF";
+    ctx.textAlign = "right";
+    for(var i=0 ; i < xlines ; i++)
+    {
+        x = PADDING + TXTZONE + i*WIDTH/xlines;
+        ctx.beginPath();
+        ctx.moveTo( x, PADDING);
+        ctx.lineTo( x, PADDING + HEIGHT);
+        ctx.stroke();
+        ctx.fillText(i + 1, x, 2 * PADDING + HEIGHT);
+        ctx.closePath();
+    }
+    x = TXTZONE;
+    for(var i=0 ; i <= ylines ; i++)
+    {
+        y = PADDING + i*HEIGHT/ylines;
+        ctx.beginPath();
+        ctx.moveTo( PADDING + TXTZONE,  y);
+        ctx.fillText(100 - i * ylines, x + 4.5, y);
+        ctx.lineTo(  TXTZONE + WIDTH - 2.5, y);
+        ctx.stroke();
+        ctx.closePath();
+        // ctx.beginPath();
+        // ctx.fillText(100 - i * ylines, x, y);
+        // ctx.closePath();
+    }
+}
--- test.js
+++ test.js
@@ -1,14 +1,11 @@
 const questionList = document.querySelector('#question-list');
 let idarr = [];
 let answers = [];
-let arrn = 0;
 let score = 0;
-let randoarr = [];
-let orig = [];
 
-// create element & render test
+// create element of a single question & render test
 function renderTest(doc){
-
+    let arrn = 0;
     let qdiv = document.createElement('div');
     let question = document.createElement('p');
     let options = document.createElement('div')
@@ -16,9 +13,9 @@ function renderTest(doc){
     question.textContent = doc.data().question;
     qdiv.appendChild(question);
 
-    idarr[arrn] = doc.id;
-    answers[arrn] = doc.data().answer;
-    arrn++;
+    idarr.push(doc.id);
+    answers.push(doc.data().answer);
+    //arrn++;
 
     for(let i = 0; i < 4; i++)
     {
@@ -43,67 +40,26 @@ function renderTest(doc){
     questionList.appendChild(qdiv);
 }
 
-
-function mix_and_split(quiz, form_length=30) {
-    console.log(quiz);
-    const qlength = quiz.length;
-    let q, temp_quiz;
-    let forms_number = Math.floor(qlength/form_length);
-    let versions = new Array(forms_number); //array of versions, each contain different questions
-
-    for(var i=0; i<forms_number; i++) {
-      versions[i] = new Array(form_length);
-      temp_quiz = quiz //to avoid duplicated questions in a version
-      for(var j=0 ; j<form_length; j++) {
-        q = Math.floor(Math.random() * temp_quiz.length);
-        versions[i][j] = temp_quiz.pop(q);
-      }
-    }
-    return versions;
-} 
-
-
-console.log('im here');
-
-/*db.collection('tests1').get().then(snapshot => {
-    let randoarr1 = []
-    snapshot.docs.forEach(doc => {
-       randoarr1.push(doc);
-    });
-
-    console.log(randoarr1);
-    randoarr = mix_and_split(randoarr1,4)[0];
-    console.log(randoarr);
-    randoarr.forEach(dc => {
-        renderTest(dc);
-    });
-});
-
-orig = [];
-db.collection('tests1').get().then(snapshot => {
-
-    snapshot.docs.forEach(doc => {
-        
-        orig.push({
-            //"idd": doc.id,
-            "answer": doc.data().answer,
-            //"question": doc.data().question,
-           // "option": doc.data().options
-        })
-    });
-});*/
-
-
-
 // getting data
 db.collection('tests1').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
+    /*snapshot.docs.forEach(doc => {
         renderTest(doc);
-    });
+    });*/
+    let test = [];
+    let temp = snapshot.docs;
+    console.log(temp);
+    let q;
+    for(let i=0; i<3 ; i++){
+        q = Math.floor(Math.random() * temp.length);
+        test.push(temp.splice(q, 1)[0]);
+    }
+    console.log(test);
+    test.forEach(question =>
+        {
+            console.log(question.data().answer);
+            renderTest(question);
+        })
 });
-
-arrn = 0;
-
 
 function getscore(i) {
     
